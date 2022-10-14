73b166cff5e6adaac951d0472eda7715bee4548e
atharv-bhadange <bhadange.atharv@gmail.com>
2022-10-14 09:11:46 +0530

    Testing in django introduction

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator funtion
+"""
+
+def add(x,y):
+    #Add x and y and return result
+    return x + y
+
+def subtract(x,y):
+    #Sub x from y and return result
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class ClacTest(SimpleTestCase):
+    """Test calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5,2)
+
+        self.assertEqual(res,7)
+    
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res,5)
